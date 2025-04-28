10ab6638b531f7e19763860e3e56a43237d59abb
Uknownevildoer <kithiiajames@gmail.com>
2025-04-28 18:10:06 +0300

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,20 @@
+--QUESTION 1
+CREATE TABLE Customer(
+  OrderID INT
+  CustomerName varchar(255)
+);
+CREATE TABLE ITEMS(
+  Products varchar(255)
+);
+
+--QUESTION 2
+CREATE TABLE Customers(
+  OrderID INT
+  CustomerName varchar(255)
+);
+
+CREATE TABLE Productdetails(
+  OrderID INT PRIMARY KEY
+  Product varchar(255) PRIMARY KEY
+  quantity INT
+);
